f4f869cd94b13c85b044c7abefa1d3cd83470fec
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2025-10-15 17:01:02 +0100

    small tweaks

--- gpt/utils.py
+++ gpt/utils.py
@@ -11,6 +11,8 @@ import torch
 # -----------------------------------------------------------------------------
 
 def set_seed(seed):
+    if seed is None:
+        return
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
